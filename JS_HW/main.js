e2598942d490005e6e3cc9ff8df9b275ec81a90d
Anton <antonnos765@gmail.com>
2017-12-11 00:04:18 +0200

    Update main.js

--- JS_HW/main.js
+++ JS_HW/main.js
@@ -1,17 +1,10 @@
-var name = prompt("Введіть ваше ім'я");
-while (name === "") {
-    alert("Ви не ввели ім'я");
-    var name = prompt("Введіть ваше ім'я");
-}
-var time = prompt("О котрій годині Ви будете вільні?");
-while (time === "") {
-    alert("Ви не ввели годину");
-    var time = prompt("О котрій годині Ви будете вільні?");
-}
-var place = "вдома";
-var event = prompt("Чим Ви любите займатися у вільний час?");
+var name = "Олег";
+var time = "завтра о другій годині";
+var place = "в центрі міста.";
+var event = prompt("Ваша улюблена справа?");
 while (event === "") {
-    alert("Ви не ввели чим ви любите займатися");
-    var event = prompt("Чим Ви любите займатися у вільний час?");
+    alert("Поле пусте!");
+    var event = prompt("Ваша улюблена справа?");
 }
-console.log(name, "сьогодні о", time, "годині буде", event, place);
+console.log(name, time, "буде", event, place);
+
