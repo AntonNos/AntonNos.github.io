799bfd2e87e47d72f2f48ade039462756d3edba8
Anton <antonnos765@gmail.com>
2018-01-03 21:36:43 +0200

    Update main.js

--- Converter/main.js
+++ Converter/main.js
@@ -1,7 +1,6 @@
 function convert() {
     "use strict";
-    const USDtoUAH = 28.1, EUROtoUAH = 33, USDtoEURO = 0.85, EUROtoUSD = 1.17, UAHtoUSD = 0.035, UAHtoEURO = 0.03;
-    
+    const UAH = 1, EURO = 33, USD = 28.1; 
     var input = document.getElementById('input_curr'), output, pick_input = document.getElementById("pick_input"), pick_output = document.getElementById("pick_output"),
     currency_input = pick_input.options[pick_input.selectedIndex].value, 
     currency_output = pick_output.options[pick_output.selectedIndex].value;
@@ -9,17 +8,17 @@ function convert() {
     if (currency_input === currency_output) {
         output = input.value;
     } else if (currency_input === 'USD' && currency_output === 'UAH') {
-        output = input.value * USDtoUAH;
+        output = input.value * USD;
     } else if (currency_input === 'EURO' && currency_output === 'UAH') {
-        output = input.value * EUROtoUAH;
+        output = input.value * EURO;
     } else if (currency_input === 'USD' && currency_output === 'EURO') {
-        output = input.value * USDtoEURO;
+        output = input.value * USD / EURO;
     } else if (currency_input === 'EURO' && currency_output === 'USD') {
-        output = input.value * EUROtoUSD;
+        output = input.value * EURO / USD;
     } else if (currency_input === 'UAH' && currency_output === 'USD') {
-        output = input.value * UAHtoUSD;
+        output = input.value * UAH / USD;
     } else if (currency_input === 'UAH' && currency_output === 'EURO') {
-        output = input.value * UAHtoEURO;
+        output = input.value * UAH / EURO;
     }
     document.getElementById('output_curr').value = output;
 }
