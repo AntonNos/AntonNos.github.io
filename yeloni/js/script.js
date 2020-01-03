4e4e658a2c3b1877809119bed513afb31248954d
Anton Nos <anos@anos.local>
2020-01-03 13:07:47 +0200

    add new pages

--- yeloni/js/script.js
+++ yeloni/js/script.js
@@ -60,3 +60,16 @@ function renderCal(themonth) {
         }
     });
 }
+
+$(".tabContent").hide();
+$("ul.tabs li:first").addClass("active").show();
+$(".tabContent:first").show();
+
+$("ul.tabs li").click(function() {
+    $("ul.tabs li").removeClass("active");
+    $(this).addClass("active");
+    $(".tabContent").hide();
+    var activeTab = $(this).find("a").attr("href");
+    $(activeTab).show();
+    return false;
+});
